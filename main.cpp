c9e98663fd059ba987d13b3102f6fdbe8ae07029
Adam Aske <adamaske@live.no>
2023-12-18 23:22:22 +0100

    vcpkg not finding glfw

--- main.cpp
+++ main.cpp
@@ -4,12 +4,9 @@
 
 int main(int argc, char* argv[]){
 
-   std::cout << "Hello World!\n";
-
-    // report version
     std::cout << "Version " << CPP_TEMPLATE_VERSION_MAJOR << "." << CPP_TEMPLATE_VERSION_MINOR << std::endl;
     if(argc < 2){
-
+        
     }
 
     return 0;
