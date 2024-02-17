e515c0e0e38a239546456ebfd305688f6f911741
Adam Aske <adamaske@live.no>
2024-02-17 19:18:05 +0100

    some work

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # CPP_Template
 
-Lightweight
+This is a template to generate ready-to-go CPP project with basic features such as simple TCP networking, Json & Yaml parsing, simple GUI (opt-in) and more
+
+Cmake is needed to build the project files
+Cmake must be in path variable
+Windows only
+scripts/generate.bat can automatically build the project with cmake 
+
+TCP Networking 
+-Use a library or code it myself?
+-Simple server
+-Simple client
+-Static library?
+
+JSON Parsing
+-Find library
+-Add to vendor
+-Incorporate into Cmake
+
--- main.cpp
+++ main.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
+#include <cstdio>
 
 #include "Config.h"
+#include "net_test.h"
+#include "Server.h"
+#include "Client.h"
 
 int main(int argc, char* argv[]){
 
-    std::cout << "Version " << CPP_TEMPLATE_VERSION_MAJOR << "." << CPP_TEMPLATE_VERSION_MINOR << std::endl;
-    if(argc < 2){
-        
-    }
+    std::cout << "Welcome to CPP_TEMPLATE version " << CPP_TEMPLATE_VERSION_MAJOR << "." << CPP_TEMPLATE_VERSION_MINOR << "\n";
+
+//Netowrking test 
+    Test();
+
+    Server();
+    
+    Client();
+
+    std::getchar();
 
     return 0;
 }
